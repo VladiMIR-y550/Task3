7dd2e0b4369bd581cd2a4e50b16370b397fbee63
VladiMIR <vmirlock@gmail.com>
2021-01-05 01:34:27 +0200

    add input name

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello World");
+        System.out.print("Please input name: ");
+        System.out.print(validationInput() + " Hello World");
+    }
+
+    static String validationInput() {
+        Scanner scan = new Scanner(System.in);
+        String input = null;
+        boolean valid = false;
+
+        while(!valid) {
+            if (scan.hasNext("[a-zA-Z]*")) {
+                input = scan.nextLine();
+                valid = true;
+            } else {
+                System.out.print("Invalid input, please input name: ");
+                scan.nextLine();
+            }
+        }
+        return input;
     }
 }
